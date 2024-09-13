61ef3ba4dfdeae8ce3609fa70476dc0acd0a6fb2
Raydiation <z5360323@ad.unsw.edu.au>
2024-09-13 13:20:47 +1000

    adding week 1 tute material

---
+++ tutes/week1/extra2.s
@@ -0,0 +1,44 @@
+	.text
+main:
+	# typically we put local variables in registers
+	# int x, y;
+	# $t0 = x
+	# $t1 = y
+	
+	# printf("Enter a number: ");
+	li	$v0, 4		#puts 4 into $v0
+	la	$a0, str	#la takes the address of str
+				# and puts it into $a0
+	syscall
+			#syscall looks into $v0, for a command.
+
+	# scanf("%d", &x);
+	li	$v0, 5
+	syscall
+	#read in value is now in $v0!!
+	
+	#but we want it in x
+	move 	$t0, $v0
+
+	# y = x * x;
+	mul	$t1, $t0, $t0
+
+	# printf("%d\n", y);
+	# printf("%d", y);
+	li 	$v0, 1
+	move	$a0, $t1 
+	syscall
+	# printf("\n");
+	li	$v0, 11
+	li	$a0, '\n'
+	syscall
+
+
+	li	$v0, 0
+	jr	$ra		#return
+
+
+
+	.data
+str:
+	.asciiz "Enter a number: "
---
+++ tutes/week1/q5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+//   int i = 0;
+//   while (i < 10) {
+//     printf("%d\n", i);
+//     i++;
+//   }
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+    printf("i after loop: %d\n", i);
+  return 0;
+}
